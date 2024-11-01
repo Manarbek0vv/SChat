dddec2e111be205a4efcdc3185649d3d60341fe3
Bekzhan Manarbekov <163629838+Manarbek0vv@users.noreply.github.com>
2024-11-01 16:50:04 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,16 +9,6 @@ import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
 import { getStorage, ref } from 'firebase/storage';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDB0vzZs8RfqHEZu7fY39sJmnSnp0xi_dE",
-  authDomain: "manarbek0v-social-network.firebaseapp.com",
-  databaseURL: "https://manarbek0v-social-network-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "manarbek0v-social-network",
-  storageBucket: "gs://manarbek0v-social-network.appspot.com",
-  messagingSenderId: "1091653592235",
-  appId: "1:1091653592235:web:6fac74d1f571d897a003ee",
-  measurementId: "G-GVPWBHHYL3"
-};
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
